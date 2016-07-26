824faca2015d5479b7f67e4c7454d17ff9c89408
Shuo Zheng <shuo.s.zheng@gmail.com>
2016-07-26 11:45:01 -0400

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import { Component, Props } from 'react';
 interface HotKeysProps extends Props<HotKeys> {
   onFocus?: Function;
   onBlur?: Function;
-  keyMap?: {[event: string]: string};
+  keyMap?: {[event: string]: string|string[]};
   handlers?: {[event: string]: Function};
   focused?: boolean;
   attach?: any;
