eb4163383a9680f52ca768ea32ba7e18f354bd90
Aleck Greenham <greenhama13@gmail.com>
2019-02-17 15:57:54 +0200

    Add Delete/Backspace test for GlobalHotkeys

---
+++ test/GlobalHotKeys/DeleteAndBackSpaceAliases.spec.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import {expect} from 'chai';
+import sinon from 'sinon';
+import simulant from 'simulant';
+
+import { GlobalHotKeys } from '../../src/';
+
+const keyAliases = ['del', 'backspace', 'Delete', 'BackSpace' ];
+const keyNames = [ 'Delete', 'Backspace' ];
+
+keyAliases.forEach(function(keyAlias) {
+  keyNames.forEach(function(keyName) {
+    describe(`When an action is defined using the combination '${keyAlias}':`, () => {
+      context(`and a ${keyName} event is triggered`, () => {
+        beforeEach(function () {
+          this.keyMap = {
+            'ACTION1': keyAlias,
+          };
+
+          this.handler = sinon.spy();
+
+          this.handlers = {
+            'ACTION1': this.handler,
+          };
+
+          this.reactDiv = document.createElement('div');
+          document.body.appendChild(this.reactDiv);
+
+          this.wrapper = mount(
+            <div>
+              <GlobalHotKeys keyMap={this.keyMap} handlers={this.handlers} />
+            </div>,
+            { attachTo: this.reactDiv }
+          );
+        });
+
+        afterEach(function() {
+          document.body.removeChild(this.reactDiv);
+        });
+
+        it('then calls the associated handler', function() {
+          simulant.fire(this.reactDiv, 'keydown', { key: keyName });
+
+          expect(this.handler).to.have.been.calledOnce;
+        });
+      });
+    })
+  })
+});
