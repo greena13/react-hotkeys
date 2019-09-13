de2c19f4b9efae794e7070e085656371a21e889d
Alex Jeffrey <alex@alexjeffrey.mee>
2019-09-13 23:10:04 +0100

    #230 - Fixed typing of HotKeysEnabled to allow it to be rendered in JSX

--- index.d.ts
+++ index.d.ts
@@ -28,7 +28,7 @@ export type KeyMap = { [key in ActionName]: KeySequence };
  * selectively triggers actions (that may be handled by handler functions) when a
  * sequence of events matches a list of pre-defined sequences or combinations
  */
-export interface HotKeysEnabled extends React.Component<HotKeysProps, {}> { }
+export type HotKeysEnabled = React.ComponentType<HotKeysProps> { }
 
 export interface GlobalHotKeysProps extends React.HTMLAttributes<HotKeys> {
   /**
